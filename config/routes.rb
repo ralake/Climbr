51c800d45dd6c40016abb6d7e3568048609d3f17
Richard Lake <ralake1985@gmail.com>
2014-12-21 16:37:24 +0000

    Add devise and first user tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -7,6 +8,8 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
+
+  root to: "posts#index"
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
---
+++ spec/features/users_feature_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'Users' do
+	
+	context "User not signed in visits the homepage" do
+
+		scenario "should see sign up and in links" do
+			visit '/posts'
+			expect(page).to have_link("Sign up")
+			expect(page).to have_link("Sign in")
+		end
+
+		scenario "should not see sign outn link" do
+			visit '/posts'
+			expect(page).not_to have_link("Sing out")
+		end
+
+	end
+
+end
