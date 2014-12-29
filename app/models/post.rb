784ed75dd0958f60caa940eefec1759d4afefb39
Richard Lake <ralake1985@gmail.com>
2014-12-29 14:45:13 +0000

    Add auto-orient to user attachments

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,14 +4,16 @@ class Post < ActiveRecord::Base
 	has_many :likes
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "500x500>" },
-	:storage => :s3,
-	:s3_credentials => {
-	  :bucket => ENV['S3_BUCKET_NAME'],
-	  :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-	  :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-	  },
-	:s3_host_name => 's3-eu-west-1.amazonaws.com'
+	has_attached_file :image,
+		:styles => { :medium => "500x500>" },
+		:convert_options => { :all => '-auto-orient' }, 
+		:storage => :s3,
+		:s3_credentials => {
+	  	:bucket => ENV['S3_BUCKET_NAME'],
+	  	:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+	  	:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+	  	},
+		:s3_host_name => 's3-eu-west-1.amazonaws.com'
 
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
